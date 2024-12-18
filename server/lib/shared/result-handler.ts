d630eddc5660038d052ff91bb9894cfc5d475a0c
Edvinas <edvinasalimas98@gmail.com>
2024-12-18 21:32:45 +0200

    Add result handler

---
+++ server/lib/shared/result-handler.ts
@@ -0,0 +1,8 @@
+import { APIGatewayProxyCallback } from "aws-lambda";
+
+export const handleResult = (callback: APIGatewayProxyCallback, result: any, statusCode: number) => {
+    callback(null, {
+        statusCode: statusCode,
+        body: JSON.stringify({ result }),
+    });
+}
