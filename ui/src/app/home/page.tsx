453bb79cd68d9acfd5943ad39906e63c5a2a4b3e
Edvinas Alimas <edvinas.alimas@devbridge.com>
2024-12-19 19:57:03 +0200

    Add initial front-end folder structure

---
+++ ui/src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/layout/Layout";
+
+export default function Main() {
+    return (
+        <div>
+            MAIN
+            <Layout/>
+        </div>
+    );
+}
