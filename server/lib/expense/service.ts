c8b3483a78fef4dd3a64be70a4d559d1a7324db7
RaminTuzelis <k.mickunas@gmail.com>
2024-12-31 17:29:57 +0200

    Added BadRequestExceptionMessage if user not found

--- server/lib/expense/service.ts
+++ server/lib/expense/service.ts
@@ -1,8 +1,15 @@
 import { Expense } from './model';
-import { postItem } from 'shared';
+import {BadRequestExceptionMessage, getByField, postItem} from 'shared';
 import { v4 as uuidv4 } from 'uuid';
 
 export const saveExpense = async (name: string, email: string, currency: string): Promise<void> => {
+
+    const user = await getByField('Users', 'email', email);
+
+    if (user.Count === 0) {
+        throw new BadRequestExceptionMessage('User not found');
+    }
+
     const expense: Expense = {
         id: uuidv4(),
         name,
