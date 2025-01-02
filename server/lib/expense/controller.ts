8d8462b9ea473502ed095156dd0c1f9204565758
RaminTuzelis <k.mickunas@gmail.com>
2025-01-02 09:42:34 +0200

    Created endpoint to retrieve created expenses with validation and message if email with that expenses not exist.

--- server/lib/expense/controller.ts
+++ server/lib/expense/controller.ts
@@ -1,6 +1,6 @@
 import { APIGatewayEvent, APIGatewayProxyCallback, Context } from "aws-lambda";
-import { handleError, handleResult } from "shared";
-import { saveExpense } from "./service";
+import {BadRequestExceptionMessage, handleError, handleResult} from "shared";
+import {getExpensesByEmail, saveExpense} from "./service";
 import {CreateExpense} from "./model";
 
 
@@ -11,6 +11,21 @@ export const handler = async (event: APIGatewayEvent, _: Context, callback: APIG
         await saveExpense(requestBody as unknown as CreateExpense);
 
         return handleResult(callback, { message: `Expense saved successfully` }, 200);
+    } catch (e) {
+        return handleError(callback, e);
+    }
+};
+
+export const getExpensesHandler = async (event: APIGatewayEvent, _: Context, callback: APIGatewayProxyCallback) => {
+    try {
+        const email = event.queryStringParameters?.email;
+        if (!email) {
+            throw new BadRequestExceptionMessage("Email is required");
+        }
+
+        const expenses = await getExpensesByEmail(email);
+
+        return handleResult(callback, { data: expenses }, 200);
     } catch (e) {
         return handleError(callback, e);
     }
