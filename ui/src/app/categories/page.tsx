f25881e28f7a5ada7d1437ecc3aebf57f0aee3c3
RaminTuzelis <k.mickunas@gmail.com>
2025-01-12 11:35:26 +0200

    Remove console

--- ui/src/app/categories/page.tsx
+++ ui/src/app/categories/page.tsx
@@ -25,7 +25,6 @@ export default async function Categories() {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/category/${cookieStore.get('email')?.value}`);
 
     const data = await response.json()
-    console.log(data);
     return (
         <Box sx={{ padding: 2 }}>
             <Typography variant="h4" gutterBottom>
