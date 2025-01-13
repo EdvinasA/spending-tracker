4918a8eb4c3a635d0f1a7d19c89d3c4574936d27
RaminTuzelis <k.mickunas@gmail.com>
2025-01-13 20:34:57 +0200

    Updated design, optimized global styles and theme.ts base colors.

--- ui/src/app/categories/page.tsx
+++ ui/src/app/categories/page.tsx
@@ -8,9 +8,10 @@ import {
     TableRow,
     Paper,
     Typography,
-    Box,
+    Box, Stack, Button,
 } from '@mui/material';
 import { cookies } from 'next/headers';
+import { format } from 'date-fns';
 
 export interface Category {
     id: string;
@@ -23,30 +24,83 @@ export interface Category {
 export default async function Categories() {
     const cookieStore = await cookies();
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/category/${cookieStore.get('email')?.value}`);
-
     const data = await response.json()
+
     return (
-        <Box sx={{ padding: 2 }}>
-            <Typography variant="h4" gutterBottom>
+        <Box sx={{ padding: '16px 16px 0' }}>
+            <Typography
+                variant="h4"
+                gutterBottom
+                sx={{
+                    color: 'text.primary',
+                    fontWeight: 'bold',
+                }}
+                >
                 Categories
             </Typography>
 
             {data &&
-                < TableContainer component={Paper}>
+                < TableContainer component={Paper} sx={{ backgroundColor: 'background.paper', borderRadius: '8px'}}>
                     <Table>
-                        <TableHead>
+                        <TableHead
+                            sx={{ backgroundColor: 'background.default' }}>
                             <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell>Created At</TableCell>
-                                <TableCell>Currency</TableCell>
-                                <TableCell>Email</TableCell>
+                                <TableCell
+                                    sx={{
+                                        color: 'text.primary',
+                                        fontWeight: 'bold',
+                                        borderBottom: '2px solid #444',
+                                    }}
+                                >
+                                    Name
+                                </TableCell>
+                                <TableCell
+                                    sx={{
+                                        color: 'text.primary',
+                                        fontWeight: 'bold',
+                                        borderBottom: '2px solid #444',
+                                    }}
+                                >
+                                    Email
+                                </TableCell>
+                                <TableCell
+                                    sx={{
+                                        color: 'text.primary',
+                                        fontWeight: 'bold',
+                                        borderBottom: '2px solid #444',
+                                    }}
+                                >
+                                    Currency
+                                </TableCell>
+                                <TableCell
+                                    sx={{
+                                        color: 'text.primary',
+                                        fontWeight: 'bold',
+                                        borderBottom: '2px solid #444',
+                                    }}
+                                >
+                                    Created At
+                                </TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
                             {data.data.map((category: Category) => (
-                                <TableRow key={category.id}>
-                                    <TableCell>{category.name}</TableCell>
-                                    <TableCell>{category.email}</TableCell>
+                                <TableRow
+                                    key={category.id}
+                                    sx={{
+                                        '&:hover': {
+                                            backgroundColor: 'primary.light',
+                                            transition: 'background-color 0.3s ease',
+                                        },
+                                        borderBottom: '1px solid #444',
+                                    }}
+                                >
+                                    <TableCell sx={{ color: 'text.primary' }}>{category.name}</TableCell>
+                                    <TableCell sx={{ color: 'text.primary' }}>{category.email}</TableCell>
+                                    <TableCell sx={{ color: 'text.primary' }}>{category.currency}</TableCell>
+                                    <TableCell sx={{ color: 'text.primary' }}>
+                                        {format(new Date(category.createdAt), 'yyyy-MM-dd')}
+                                    </TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
