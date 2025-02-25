cf9c18fb3ddcbcb66127adb2a9f67b975d6361b7
RaminTuzelis <k.mickunas@gmail.com>
2025-02-25 20:00:26 +0200

    Delete redundant validation because we validate with JOI

--- server/lib/category/delete-category/controller.ts
+++ server/lib/category/delete-category/controller.ts
@@ -9,10 +9,6 @@ export const handler = async (event: APIGatewayEvent, _: Context, callback: APIG
         const categoryId = event.pathParameters?.categoryId ?? "";
         const email = event.queryStringParameters?.email ?? "";
 
-        if (!categoryId || !email) {
-            return handleError(callback, new Error("Missing categoryId or email"));
-        }
-
         const requestBody: DeleteCategoryRequest = { categoryId, email };
         const service = new DeleteCategoryService();
 
