77716f5ce2877521d05bd38fd8cf8e3b26c41765
Edvinas <edvinasalimas98@gmail.com>
2025-03-10 11:33:54 +0200

    Update deployed branch

--- server/lib/authorizer/controller.ts
+++ server/lib/authorizer/controller.ts
@@ -3,9 +3,12 @@ import { AuthorizerService } from './authorizer-service';
 
 export async function handler(event: APIGatewayRequestAuthorizerEventV2, _: Context) {
   try {
+    console.log(event);
     const token = event.headers?.authorization?.split(' ')[1];
 
     const authorizerService = new AuthorizerService();
+
+    console.log(authorizerService.validateToken(token!));
 
     return { isAuthorized: authorizerService.validateToken(token!) }
   } catch (error) {
