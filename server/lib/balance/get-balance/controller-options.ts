7c8ef765fef20e04bb322205a22fe49632deaead
Edvinas <edvinasalimas98@gmail.com>
2025-03-10 11:17:34 +0200

    Revert not needed changes

--- server/lib/balance/get-balance/controller-options.ts
+++
@@ -1,6 +0,0 @@
-import { APIGatewayEvent, APIGatewayProxyCallback, Context } from "aws-lambda";
-import { handleOptionsResult } from "shared";
-
-export const handler = async (__: APIGatewayEvent, _: Context, callback: APIGatewayProxyCallback) => {
-    return handleOptionsResult(callback, 200);
-};
